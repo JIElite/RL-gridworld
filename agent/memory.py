5ba3c519a49502591f2b63d269400b662c1fccf6
JIElite <ita3051@gmail.com>
2018-01-02 22:28:36 +0800

    add memory

--- agent/memory.py
+++ agent/memory.py
@@ -1,12 +1,35 @@
-class IMemorizable:
-    def __init__(self):
-        self.init_memory()
+import random
+from collections import namedtuple, deque
+from abc import abstractmethod
 
-    def init_memory(self):
+
+Transition = namedtuple('Transition', ['s', 'a', 'r', 's_', 'alive'])
+
+
+class Memory:
+    @abstractmethod
+    def memorize(self, experience):
         pass
 
-    def memorize(self):
+    @abstractmethod
+    def sample(self, sample_size=32):
         pass
 
-    def replay(self):
-        pass
+
+class DeepQReplayBuffer(Memory):
+    def __init__(self, capacity=10000):
+        self.capacity = capacity
+        self.memory = deque(maxlen=self.capacity)
+
+    def memorize(self, experience):
+        self.memory.append(Transition(*experience))
+
+    def sample(self, sample_size=32):
+        current_length = len(self.memory)
+        if current_length < sample_size:
+            sample_size = current_length
+
+        sampled_experience = random.sample(self.memory, sample_size)
+        return Transition(*zip(*sampled_experience))
+
+
