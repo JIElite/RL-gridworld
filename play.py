2209895572f6ca860e1e1cfb78040f05bcc6f404
JIElite <ita3051@gmail.com>
2018-01-02 22:30:43 +0800

    update play.py

--- play.py
+++ play.py
@@ -1,4 +1,6 @@
 from agent.agent import RandomDiscreteAgent
+from agent.DeepQ import DeepQAgent
+from agent.A2C import A2CAgent
 from maze.simple_maze import SimpleMaze
 
 
@@ -8,15 +10,19 @@ N_ACTIONS = env.get_action_space()[1][0]
 MAX_EPISODES = 200
 MAX_STEPS = 200
 
-agent = RandomDiscreteAgent(n_actions=N_ACTIONS)
+# agent = DeepQAgent(n_actions=N_ACTIONS, n_states=N_STATES, epsilon=0.1)
+agent = A2CAgent(n_actions=N_ACTIONS, n_states=N_STATES)
+# agent = RandomDiscreteAgent(n_actions=N_ACTIONS)
+agent.load_model()
+
 for i_episode in range(MAX_EPISODES):
     s = env.reset()
     for step in range(1, MAX_STEPS+1):
-        # env.render()
+        env.render()
         action = agent.select_action(s)
         s_, reward, done = env.step(action)
 
         if done or step == MAX_STEPS:
-            # env.render()
+            env.render()
             print('Episode:{0} done. step:{1}'.format(i_episode, step))
             break
